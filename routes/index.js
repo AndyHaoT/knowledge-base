6e1f2fd23696d9974021b30bfb00e906d5381d1f
Siya Peng <ppsy.loves@gmail.com>
2019-11-30 15:38:41 -0800

    thread and conversation pages

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@ const SearchController = require('../controllers/SearchController');
 const PostController = require('../controllers/PostController');
 const UserProfileController = require('../controllers/UserProfileController');
 const HomePageController = require('../controllers/HomePageController');
+const ThreadController = require('../controllers/ThreadController');
+const ConversationController = require('../controllers/ConversationController');
 const router = express.Router();
 
 router.get('/', LoginController.root_get);
@@ -30,6 +32,16 @@ router.post('/post/:post_id/add', PostController.addComment);
 router.get('/profile/:user_id', UserProfileController.loadProfile);
 
 router.get('/profile/:user_to_like/like', UserProfileController.likeUser);
+
+router.get('/thread/:receiver_id', ThreadController.newThread);
+
+router.post('/thread/post/:receiver_id', ThreadController.createThread);
+
+router.get('/conversation', ConversationController.getConversations);
+
+router.get('/conversation/:thread_id', ConversationController.getConversation);
+
+router.post('/conversation/:thread_id/post/:receiver_id/message/:message', ConversationController.writeMessage);
 
 router.get('/home', HomePageController.getHomePage);
 
